1279cb3c492cd3f0dc7ceb40ef13d749ee001fb0
Swarzstein <zorrillagja@gmail.com>
2023-01-25 18:27:17 -0400

    Actions changed following the ducks pattern

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,15 +1,18 @@
-export default function booksReducer(state = {}, action) {
+const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+
+export default function booksReducer(state = [], action) {
   switch (action.type) {
-    case 'ADD_BOOK': return {};
-    case 'REMOVE_BOOK': return {};
+    case ADD_BOOK: return {};
+    case REMOVE_BOOK: return {};
     default: return state;
   }
 }
 
 export function addBook() {
-  return { type: 'ADD_BOOK' };
+  return { type: ADD_BOOK };
 }
 
 export function removeBook() {
-  return { type: 'REMOVE_BOOK' };
+  return { type: REMOVE_BOOK };
 }
--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,10 +1,12 @@
-export default function categoriesReducer(state = {}, action) {
+const CHECK_STATUS = 'bookstore/categories/CHECK_STATUS';
+
+export default function categoriesReducer(state = [], action) {
   switch (action.type) {
-    case 'CHECK_STATUS': return 'Under construction';
+    case CHECK_STATUS: return 'Under construction';
     default: return state;
   }
 }
 
 export function checkStatus() {
-  return { type: 'CHECK_STATUS' };
+  return { type: CHECK_STATUS };
 }
